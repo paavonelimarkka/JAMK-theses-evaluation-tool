1ddec748be0b50dd5d5a80ace885c4bba351ecd7
Paavo Nelimarkka <paavo.nelimarkka@gmail.com>
2017-04-13 13:43:50 +0300

    add pure-css

--- src/feedback_generator_amk.php
+++ src/feedback_generator_amk.php
@@ -7,6 +7,7 @@ include("amk_criteria.php");
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styles/pure-css/pure-min.css">
 	<link rel="stylesheet" href="styles.css">
 	<title>SahKa feedback generator</title>
 </head>
